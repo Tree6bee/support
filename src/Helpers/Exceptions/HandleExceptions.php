ca55ea630dd40b781bb947749e5cb80bfa97f9f2
sh7ning <sh7ning@qq.com>
2016-11-17 17:20:51 +0800

    增加异常接管和输出到错误页

---
+++ src/Helpers/Exceptions/HandleExceptions.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Tree6bee\Support\Helpers\Exceptions;
+
+use ErrorException;
+use Closure;
+
+/**
+ * 框架系统异常错误处理接管类
+ * 参考 Laravel vendor/laravel/framework/src/Illuminate/Foundation/Bootstrap/HandleExceptions.php
+ *
+ * @copyright sh7ning 2016.1
+ * @author    sh7ning
+ * @version   0.0.1
+ *
+ * @example
+ * $handler = new Handler('CtxFramework/1.1');
+ * new HandleExceptions(true, function ($e) use ($handler) {
+ *      call_user_func([$handler, 'handle'], $e);
+ * });
+ */
+class HandleExceptions
+{
+    /**
+     * @var $debug
+     */
+    protected $debug;
+
+    protected $callback;
+
+    public function __construct($debug, Closure $callback)
+    {
+        //初始化
+        $this->debug = $debug;
+        $this->callback = $callback;
+        //接管
+        $this->handle();
+    }
+
+    protected function handle()
+    {
+        error_reporting(-1);
+
+        if ($this->debug) {
+            ini_set('display_errors', 'On');
+        } else {
+            //--@todo 待验证 不建议配置nginx的500,400等多种错误页，否则没法接管--
+            ini_set('display_errors', 'Off');
+        }
+
+        //错误捕获
+        set_error_handler(array($this, 'handleError')); //接收所有的错误类型
+
+        //异常捕获
+        set_exception_handler(array($this, 'handleException'));
+
+        //程序结束 第一个参数为回调，后续都是作为回调函数的参数
+        register_shutdown_function(array($this, 'handleShutdown'), 0);
+    }
+
+    /**
+     * 错误处理接管
+     * 一般用于捕捉  E_NOTICE 、E_USER_ERROR、E_USER_WARNING、E_USER_NOTICE (trigger_error可以触发)
+     * 不能捕捉: E_ERROR、 E_PARSE、 E_CORE_ERROR、 E_CORE_WARNING、 E_COMPILE_ERROR、 E_COMPILE_WARNING
+     *          和在 调用 set_error_handler() 函数所在文件中产生的大多数 E_STRICT
+     *
+     * momo
+     * $curerrorno = error_reporting();
+     * if (($curerrorno & ~$errorno) == $curerrorno) {
+     *     return true;
+     * }
+     */
+    public function handleError($errorno, $errorstr, $errorfile, $errorline)
+    {
+        if (error_reporting() & $errorno) {
+            throw new ErrorException($errorstr, 0, $errorno, $errorfile, $errorline);
+        }
+    }
+
+    /**
+     * 运行结束 或 致命错误退出 捕获
+     */
+    public function handleShutdown($status = 0)
+    {
+        // if (connection_aborted()) { //@todo 增加对用户主动取消的日志收集
+        // }
+
+        if (! is_null($error = error_get_last()) && self::isFatal($error['type'])) {
+            return $this->handleException(
+                new ErrorException($error['message'], $error['type'], 0, $error['file'], $error['line'])
+            );
+        }
+    }
+
+    /**
+     * Determine if the error type is fatal.
+     * E_USER_ERROR E_RECOVERABLE_ERROR:
+     */
+    protected function isFatal($type)
+    {
+        return in_array($type, array(E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE));
+    }
+
+    /**
+     * 异常处理接管
+     */
+    public function handleException($e)
+    {
+        call_user_func($this->callback, $e);
+    }
+}
---
+++ src/Helpers/Exceptions/Handler.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Tree6bee\Support\Helpers\Exceptions;
+
+use Tree6bee\Support\Helpers\Exceptions\Reporter\Debuger;
+
+class Handler
+{
+    protected $cfVersion;
+
+    public function __construct($cfVersion = 'CtxFramework/1.0')
+    {
+        $this->cfVersion = $cfVersion;
+    }
+
+    /**
+     * 异常接管
+     * 1. 记录框架异常日志
+     * 2. 展示:
+     *      * 命令行:直接输出
+     *      * http方式:
+     *          * 测试环境：输出错误页面
+     *          * 其它环境(如 正式环境)：
+     *              * 框架错误：框架处理
+     *              * 其它：直接返回500错误
+     */
+    public function handle($e)
+    {
+        //有可能为 Exception 也有可能为 Throwable 需要进行转化为 Exception
+        // 如果没有这两个函数的调用，那么在后续的错误处理过程中，当再次产生异常或是错误时，可能造成死循环
+        restore_error_handler();
+        restore_exception_handler();
+
+        $ex = $this->getLogOfException($e);
+        $this->report($ex, 'exception');
+
+        $this->render($e, $ex);
+    }
+
+    protected function render($e, $ex = '')
+    {
+        if (php_sapi_name() == 'cli') { //命令行模式
+            echo "\nerror trace:\n" .
+                print_r(array_slice($e->getTrace(), 0, 6), true) . "\n";
+            return true;
+        } else {    //web运行方式
+            if ($this->wantsJson()) {
+                //@todo
+            } else {
+                return (new Debuger($this->cfVersion))->displayException($e);
+            }
+        }
+    }
+
+    /**
+     * 获取记录日志用的异常字符串
+     */
+    protected function getLogOfException($e)
+    {
+        return '(' . get_class($e) . ':' . $e->getCode() . ') ' .
+            $e->getMessage() . ' in ' . $e->getFile() . ' on line ' . $e->getLine();
+    }
+
+    /**
+     * 判断是否需要返回json
+     *
+     * @todo 完善后迁移到request中
+     * 判断 header中Content-Type是否包含'/json' 或 '+json'
+     * 判断是否ajax请求 XMLHttpRequest
+     */
+    protected function wantsJson()
+    {
+        return false;
+    }
+
+    /**
+     * 错误日志记录
+     */
+    protected function report($ex, $fileName = 'error', $cutDate = true)
+    {
+        //附加时间
+        $content = '[' . date('Y-m-d H:i:s') . ' ' . date_default_timezone_get() . '] ';
+        //错误信息
+        $content .= $ex;
+        //附加uri
+        $request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : 'Unkown';
+        $content .= ' [' . $request_uri . ']' .PHP_EOL;
+
+        //根据情况决定是否记录超全局变量，方便排查用户访问错误
+        // gethostname();   //服务器主机名，方便排查集群中的具体机器错误
+        // $GLOBALS $_SERVER $_REQUEST $_POST $_GET $_FILES $_ENV $_COOKIE $_SESSION
+        // $_SERVER['REQUEST_URI'] $_SERVER['SCRIPT_NAME'] $_SERVER['HTTP_REFERER']
+        // var_export($_SERVER, true);
+        // var_export($_COOKIE, true);
+        // var_export($_REQUEST, true);
+
+        //@todo 
+        // return Log::write($content, $fileName, $cutDate);
+    }
+}

f85532bb81b6576d1ba1c5f2e41b2cf3007e96c1
sh7ning <sh7ning@qq.com>
2017-10-19 22:18:08 +0800

    更新namespace和注释

--- src/Helpers/Loader/Psr4.php
+++ src/Helpers/Loader/Psr4.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Tree6bee\Support\Ctx\Loader;
+namespace Tree6bee\Support\Helpers\Loader;
 
 /**
  * 框架加载类
- *
+ * @deprecated 不建议采用，多依赖composer本身的类加载
  * @example 
- *      new \Tree6bee\Support\Ctx\Loader\Psr4(array('Ctx\\' => __DIR__ . '/../Ctx'));
+ *      new \Tree6bee\Support\Helpers\Loader\Psr4(array('Ctx\\' => __DIR__ . '/../Ctx'));
  */
 class Psr4
 {
